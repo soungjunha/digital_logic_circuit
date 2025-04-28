5cdbb1398a2122350a3e3ed1c19c0c1da9ee7f42
soungjun <soungjun2018@gmail.com>
2025-04-29 00:58:24 +0900

    Update Quine_McCluskey.cpp

--- Quine_McCluskey.cpp
+++ Quine_McCluskey.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<string>
+#include<fstream>
+#include<cmath>
+#include <vector>
 
 using namespace std;
 
@@ -24,5 +27,24 @@ Term::~Term()
 }
 
 int main() {
+
+    ifstream input(".\\input_minterm.txt");
+    string line;
+
+    getline(input, line);
+
+    int bit = stoi(line);
+    vector<Term> terms;
+
+    cout << bit << endl;
+    if (input.is_open()) {
+        while (getline(input, line)) {
+            cout << line << endl;
+        }
+        input.close();
+    }
+    else {
+        cerr << "Error: Failed to open file 'input_minterm.txt'" << endl;
+    }
 
 }
