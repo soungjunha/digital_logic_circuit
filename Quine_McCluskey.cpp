3b0a47f6adfe1c4ff0fa969fb76a1bad12b0e35c
soungjun <soungjun2018@gmail.com>
2025-04-28 16:24:19 +0900

    Create Quine_McCluskey.cpp

---
+++ Quine_McCluskey.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Term
+{
+public:
+	Term();
+	~Term();
+
+private:
+	bool is_minterm;
+	string term;
+
+};
+
+Term::Term()
+{
+}
+
+Term::~Term()
+{
+}
+
+int main() {
+
+}
