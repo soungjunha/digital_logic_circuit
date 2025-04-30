f29539dbb5a6c64b9a867c00fb18ed7f8a63be98
soungjun <soungjun2018@gmail.com>
2025-04-30 16:05:39 +0900

    Update Quine_McCluskey.cpp

--- Quine_McCluskey.cpp
+++ Quine_McCluskey.cpp
@@ -9,42 +9,70 @@ using namespace std;
 class Term
 {
 public:
-	Term();
-	~Term();
+    Term(bool is_minterm, string term) :is_minterm(is_minterm), term(term) {}
+    ~Term();
+    string get_term() { return term; }
+    //bool operator==(const Term& other);
 
 private:
-	bool is_minterm;
-	string term;
+    bool is_minterm;
+    string term;
 
 };
-
-Term::Term()
-{
-}
 
 Term::~Term()
 {
 }
 
+class Group
+{
+public:
+    Group();
+    ~Group();
+
+private:
+    string term;
+    vector<int> minterms;
+    bool only_dont_care;
+};
+
+Group::Group()
+{
+}
+
+Group::~Group()
+{
+}
+
 int main() {
 
-    ifstream input(".\\input_minterm.txt");
+    ifstream infile(".\\input_minterm.txt");
     string line;
 
-    getline(input, line);
+    getline(infile, line);
 
     int bit = stoi(line);
     vector<Term> terms;
+    vector<Group> groups;
 
     cout << bit << endl;
-    if (input.is_open()) {
-        while (getline(input, line)) {
-            cout << line << endl;
+    if (infile.is_open()) {
+        while (getline(infile, line)) {
+            Term buffer(line[0] == 'm', line.substr(2));
+            terms.push_back(buffer);
         }
-        input.close();
+        infile.close();
     }
     else {
         cerr << "Error: Failed to open file 'input_minterm.txt'" << endl;
+    }
+
+    for (int i = 0; i < terms.capacity(); i++)
+    {
+        for (int j = i + 1; j < terms.capacity(); j++)
+        {
+
+        }
     }
 
 }
